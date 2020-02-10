e17fe30f4217b47da97606f8421fa08ba8636eed
fbzr <fabricio.spb@gmail.com>
2020-02-10 14:29:47 -0800

    style timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -5,7 +5,7 @@ link.rel = 'stylesheet';
 document.head.appendChild(link);
 // Change font
 document.body.style.fontFamily = "'Orbitron', sans-serif";
-
+// Give width to digits except #colon
 document.querySelectorAll('.digit:not(#colon)').forEach(element => {
     element.textContent = 0;
     element.style.width = '70px';
@@ -47,8 +47,31 @@ const onButtonClick = (e) => {
     e.target.disabled = true;
 }
 
+// Create start button
+const buttonContainer = document.createElement('div');
+buttonContainer.style.width = '100%';
 const startButton = document.createElement('button');
 startButton.textContent = 'Start';
 startButton.addEventListener('click', onButtonClick);
+startButton.style.width = '170px';
+startButton.style.height = '50px';
+buttonContainer.appendChild(startButton);
+buttonContainer.style.display = 'flex';
+buttonContainer.style.justifyContent = 'center';
+buttonContainer.style.alignContent = 'center';
 
-document.querySelector('body').prepend(startButton);
+const digitsContainer = document.querySelector('.digits');
+digitsContainer.appendChild(buttonContainer);
+// Style container
+document.body.style.display = 'flex';
+document.body.style.height = '100vh';
+document.body.style.justifyContent = 'center';
+document.body.style.alignContent = 'center';
+digitsContainer.style.backgroundColor = '#666';
+digitsContainer.style.width = '50%';
+digitsContainer.style.height = '50vh';
+digitsContainer.style.display = 'flex';
+digitsContainer.style.flexWrap = 'wrap';
+digitsContainer.style.justifyContent = 'center';
+digitsContainer.style.alignContent = 'center';
+// document.querySelector('body').prepend(startButton);
